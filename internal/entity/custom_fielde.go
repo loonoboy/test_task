65dc15268c471681b8c637fa4ee024be65478156
loonoboy <tzimlov.max@yandex.ru>
2025-10-23 12:11:36 +0300

    fix Values

--- internal/entity/custom_fielde.go
+++ internal/entity/custom_fielde.go
@@ -1,13 +1,15 @@
 package entity
 
 type CustomField struct {
-	FieldID   int    `json:"field_id"`
-	FieldName string `json:"field_name"`
-	FieldCode string `json:"field_code"`
-	FieldType string `json:"field_type"`
-	Values    []struct {
-		Value    string `json:"value"`
-		EnumID   int    `json:"enum_id"`
-		EnumCode string `json:"enum_code"`
-	} `json:"values"`
+	FieldID   int      `json:"field_id"`
+	FieldName string   `json:"field_name"`
+	FieldCode string   `json:"field_code"`
+	FieldType string   `json:"field_type"`
+	Values    []Values `json:"values"`
+}
+
+type Values struct {
+	Value    string `json:"value"`
+	EnumID   int    `json:"enum_id"`
+	EnumCode string `json:"enum_code"`
 }
