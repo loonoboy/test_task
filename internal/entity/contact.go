d16afce11f32d4e9d8d04659bd51b5d5fe261cea
loonoboy <tzimlov.max@yandex.ru>
2025-10-28 10:26:53 +0300

    fix contact

--- internal/entity/contact.go
+++ internal/entity/contact.go
@@ -4,7 +4,7 @@ type Contact struct {
 	ContactID int    `json:"contact_id" validate:"required" gorm:"primaryKey;autoIncrement:false"`
 	AccountID int    `json:"account_id" validate:"required" gorm:"not null;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name      string `json:"name" validate:"required" gorm:"type:varchar(255) CHARACTER SET utf8mb4;not null"`
-	Email     string `json:"email" validate:"required" gorm:"type:varchar(255) CHARACTER SET utf8mb4;not null"`
+	Email     string `json:"email" validate:"required" gorm:"type:varchar(255) CHARACTER SET utf8mb4;not null;unique"`
 }
 
 type ContactsResp struct {
